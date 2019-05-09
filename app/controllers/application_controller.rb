9333c6b981b35f16b2e081c346b0686b401293ce
takayukihayashi <musha.musha.gokun@gmail.com>
2019-05-09 12:42:18 +0000

    Finish static page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
-end
+<!DOCTYPE html>
+<html>
+  <head>
+    <title><%= full_title(yield(:title)) %></title>
+    <%= csrf_meta_tags %>
+    <%= stylesheet_link_tag    'application', media: 'all',
+                               'data-turbolinks-track': 'reload' %>
+    <%= javascript_include_tag 'application',
+                               'data-turbolinks-track': 'reload' %>
+  </head>
+  <body>
+    <%= yield %>
+  </body>
+</html>
