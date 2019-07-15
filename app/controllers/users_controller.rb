abb35e6006d39058f2855372607054c0a1e71015
takayukihayashi <musha.musha.gokun@gmail.com>
2019-07-15 11:58:13 +0000

    ensure_correct_user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
   before_action :admin_user,     only: [:destroy, :edit_basic_info, :update_basic_info]
+  before_action :ensure_correct_user,   only: [:edit, :update, :show]
   
   def index
    @users = User.paginate(page: params[:page]).search(params[:search])
@@ -102,6 +103,13 @@ class UsersController < ApplicationController
     # 管理者かどうか確認
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+    
+    def ensure_correct_user
+      if current_user.id != params[:id].to_i && !current_user.admin?
+        flash[:danger] = "権限がありません"
+        redirect_to root_path
+      end
     end
     
 
